88fd26ece56d910dc00dc3f6401f4e9aee9e4444
Shelton17 <okangajeff17@gmail.com>
2024-01-25 21:09:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/whatsappclone/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.whatsappclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.whatsappclone.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        super.onCreate(savedInstanceState)
+        setContentView(view)
+
+
+    }
+}
